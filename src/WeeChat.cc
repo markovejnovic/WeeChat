99f07471eeec1cb0b75c4c1756bc53c58b450692
Marko Vejnovic <marko.vejnovic@hotmail.com>
2017-10-19 22:23:04 +0800

    Created a .glade interface file. Started working on the interface. Added Makefile rules for data

--- src/WeeChat.cc
+++ src/WeeChat.cc
@@ -2,14 +2,21 @@
 #include "Configuration.h"
 #include "ConsolePrinter.h"
 
+Gtk::ApplicationWindow *appWindow = nullptr;
+
 int createApp(int argc, char **argv, Config::ConfigurationValues vals) {
-	Glib::RefPtr<Gtk::Application> app =
-	Gtk::Application::create(argc, argv, "org.gtkmm.examples.base");
+	Glib::RefPtr<Gtk::Application> app = Gtk::Application::create(argc, argv, "org.gtkmm.examples.base");
 
-	Gtk::Window window;
-	window.set_default_size(vals.resolution.first, vals.resolution.second);
+	Glib::RefPtr<Gtk::Builder> uiBuilder = Gtk::Builder::create();
+	uiBuilder->add_from_file(DATADIR "/ui.glade");
 
-	return app->run(window);
+	uiBuilder->get_widget("MainApplicationWindow", appWindow);
+
+	if (appWindow) {
+		return app->run(*appWindow);
+	} else {
+		return -2;
+	}
 }
 
 int main(int argc, char **argv)
@@ -21,8 +28,13 @@ int main(int argc, char **argv)
 		Config::create();
 
 		return createApp(argc, argv, Config::read());
+	} catch (const Glib::FileError &fe) {
+		Console::lErr("No Glade UI File found. Exiting.", fe.what());
+	} catch (const Glib::MarkupError &me) {
+		Console::lErr("Bad Glade UI File Markup. Exiting.", me.what());
+	} catch (const Gtk::BuilderError &be) {
+		Console::lErr("There's a builder error", be.what());
 	}
-
 
 	return -1;
 }
